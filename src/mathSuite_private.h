88f3318e0c8a658d7947bc101f89582c02ccf75b
Marco Chiarelli <marco_chiarelli@yahoo.it>
2014-09-11 00:01:01 +0200

    Revert "mathSuite v6.50 - Final commi"
    This reverts commit c053c12463b4ee9894b0853e619438788df33adc.

--- src/mathSuite_private.h
+++ src/mathSuite_private.h
@@ -5,19 +5,19 @@
 #define MATHSUITE_PRIVATE_H
 
 /* VERSION DEFINITIONS */
-#define VER_STRING	"6.5.0.394"
+#define VER_STRING	"6.0.1.390"
 #define VER_MAJOR	6
-#define VER_MINOR	5
-#define VER_RELEASE	0
-#define VER_BUILD	394
+#define VER_MINOR	0
+#define VER_RELEASE	1
+#define VER_BUILD	390
 #define COMPANY_NAME	"NextGenLab - OBSIDIAN Environmenting"
-#define FILE_VERSION	"6.5.0.394"
+#define FILE_VERSION	"6.0.1.390"
 #define FILE_DESCRIPTION	"mathSuite"
 #define INTERNAL_NAME	"mSuite"
 #define LEGAL_COPYRIGHT	"Protected by CC BY-SA 2.0"
 #define LEGAL_TRADEMARKS	"Protected by CC BY-SA 2.0"
 #define ORIGINAL_FILENAME	"mathSuite v6.00"
 #define PRODUCT_NAME	"mathSuite"
-#define PRODUCT_VERSION	"6.5.0.394"
+#define PRODUCT_VERSION	"6.0.1.390"
 
 #endif /*MATHSUITE_PRIVATE_H*/
